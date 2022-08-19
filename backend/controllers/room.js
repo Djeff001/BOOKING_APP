806b86ef1d43892e7f58f01b5cb5ff9a7e1f1cc2
Djeff <nait.sider.djaffer@gmail.com>
2022-08-19 20:07:58 +0200

    add and delete room

--- backend/controllers/room.js
+++ backend/controllers/room.js
@@ -29,8 +29,12 @@ export const updateRoom = async (req, res, next) => {
 };
 
 export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelId;
   try {
     await Room.findByIdAndDelete(req.params.id);
+    await Hotel.findByIdAndUpdate(hotelId, {
+      $pull: { rooms: req.params.id },
+    });
     res.status(200).json("Room has been deleted");
   } catch (err) {
     next(err);

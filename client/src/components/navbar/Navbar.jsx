5dd244b5ceab4bc646315ff2026c067272dfec85
Djeff <nait.sider.djaffer@gmail.com>
2022-08-27 16:57:54 +0200

    add logout logo

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,10 +2,20 @@ import "./navbar.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext.js";
+import axios from "axios";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { user } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleClick = async () => {
+    try {
+      await axios.get("/auth/logout/");
+      dispatch({ type: "LOGOUT" });
+      navigate("/");
+    } catch (err) {}
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
@@ -13,7 +23,15 @@ const Navbar = () => {
           <span className="logo">Mybooking</span>
         </Link>
         {user ? (
-          user.username
+          <div className="logout">
+            {user.username}
+            <img
+              onClick={handleClick}
+              src="./img/logout.svg"
+              alt="logout"
+              className="navButton"
+            />
+          </div>
         ) : (
           <div className="navItems">
             <button onClick={() => navigate("/register")} className="navButton">
